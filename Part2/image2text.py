fixed bug in log prob calculation in  nb

Part2/image2text.py
@@ -91,7 +91,7 @@ def nb_classifier(mtx,train_mtx,classes):
     NUM_CLASSES = len(classes)
     score = {}
     for c in range(NUM_CLASSES):
-        log_prob = -1*np.log(1/NUM_CLASSES)+(np.sum(np.equal(mtx[0,:],train_mtx[c,:]))*np.log(m))+(np.sum(np.not_equal(mtx[0,:],X[c,:]))*np.log(1-m))
+        log_prob = -1*np.log(1/NUM_CLASSES)+(np.sum(np.equal(mtx[0,:],train_mtx[c,:]))*np.log(m))+(np.sum(np.not_equal(mtx[0,:],train_mtx[c,:]))*np.log(1-m))
         score[classes[c]]=log_prob
     return score
 